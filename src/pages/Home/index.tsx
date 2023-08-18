1a98a75eaceff391a3bc04d79e074848e579d3a8
Julio <julioaraujo@MacBook-Pro-de-Julio.local>
2023-08-17 22:13:18 -0300

    config eslint rocketseat with prettier and tailwindcss

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,25 +7,25 @@ import {
 } from 'react-icons/bi'
 import { SiNextdotjs } from 'react-icons/si'
 import { IoLogoHtml5 } from 'react-icons/io'
-import { RiJavascriptFill } from 'react-icons/ri';
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { RiJavascriptFill } from 'react-icons/ri'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 const Home = () => {
   return (
     <div className="relative min-h-[100vh]">
       <header
-        className="pt-14 flex items-center flex-wrap 
-            smPhone:containerMySpaceDeviceHand 
+        className="smPhone:containerMySpaceDeviceHand smLaptop:containerMyspace flex flex-wrap  
+            items-center 
+            pt-14 
             smPhone:flex-col 
             smPhone:justify-center 
             smPhone:gap-7 
-            smLaptop:containerMyspace 
             smLaptop:flex-row 
             smLaptop:justify-between
             "
       >
         <div>
-          <h3 className="smPhone:text-xl mdPhone:text-2xl font-bold tracking-widest">
+          <h3 className="font-bold tracking-widest smPhone:text-xl mdPhone:text-2xl">
             Julio
             <span className="text-blue-violet-default mdPhone:text-[2rem]">
               {' { '}
@@ -38,7 +38,7 @@ const Home = () => {
         </div>
 
         <div
-          className="text-2xl flex items-center gap-3 tracking-wider 
+          className="flex items-center gap-3 text-2xl tracking-wider 
                 smPhone:flex-col smPhone:justify-center
                 smLaptop:flex-row
                 "
@@ -49,7 +49,7 @@ const Home = () => {
           <a
             download
             href="/src/assets/juliochiuchi-cv.pdf"
-            className="text-lg border rounded-md border-blue-violet-default p-3 hover:bg-blue-violet-default hover:text-white-ice transition-colors duration-700 ease-in-out animate-pulse smLaptop:ml-8"
+            className="animate-pulse rounded-md border border-blue-violet-default p-3 text-lg transition-colors duration-700 ease-in-out hover:bg-blue-violet-default hover:text-white-ice smLaptop:ml-8"
           >
             download cv
           </a>
@@ -57,10 +57,10 @@ const Home = () => {
       </header>
 
       <div
-        className="mt-16 pb-[4rem] 
-            smPhone:containerMyspaceDeviceHand 
-            smPhone:text-center
-            smLaptop:containerMyspace 
+        className="smPhone:containerMyspaceDeviceHand smLaptop:containerMyspace 
+            mt-16 
+            pb-[4rem]
+            smPhone:text-center 
             smLaptop:text-left
             "
       >
@@ -69,11 +69,11 @@ const Home = () => {
         </div>
 
         <div
-          className="smLaptop:w-3/4
-                smPhone:pl-8
+          className="smPhone:pl-8
                 smPhone:pr-8
                 mdPhone:pl-0
                 mdPhone:pr-0
+                smLaptop:w-3/4
                 "
         >
           <p
@@ -110,54 +110,54 @@ const Home = () => {
                 smLaptop:justify-start
                 "
         >
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <FaReact className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <FaReact className="h-10 w-10" />
             <span>REACT</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <BiLogoTypescript className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <BiLogoTypescript className="h-10 w-10" />
             <span>TYPESCRIPT</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <BiLogoTailwindCss className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <BiLogoTailwindCss className="h-10 w-10" />
             TAILWIND
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <BiLogoNodejs className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <BiLogoNodejs className="h-10 w-10" />
             <span>NODE</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <SiNextdotjs className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <SiNextdotjs className="h-10 w-10" />
             <span>NEXT</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <FaReact className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <FaReact className="h-10 w-10" />
             <span>REACT NATIVE</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <BiLogoGit className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <BiLogoGit className="h-10 w-10" />
             <span>GIT</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <IoLogoHtml5 className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <IoLogoHtml5 className="h-10 w-10" />
             <span>HTML</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <FaCss3Alt className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <FaCss3Alt className="h-10 w-10" />
             <span>CSS</span>
           </div>
-          <div className="p-5 border border-blue-violet-default rounded-md min-w-32 w-36 text-center text-sm flex flex-col gap-2 items-center">
-            <RiJavascriptFill className="w-10 h-10" />
+          <div className="min-w-32 flex w-36 flex-col items-center gap-2 rounded-md border border-blue-violet-default p-5 text-center text-sm">
+            <RiJavascriptFill className="h-10 w-10" />
             <span>JAVASCRIPT</span>
           </div>
         </div>
       </div>
 
       <footer
-        className="pb-[1rem] bottom-0 gap-7 flex items-center 
-            smPhone:containerMyspaceDeviceHand 
+        className="smPhone:containerMyspaceDeviceHand smLaptop:containerMyspace bottom-0 flex items-center 
+            gap-7 
+            pb-[1rem] 
             smPhone:justify-center 
-            smLaptop:containerMyspace 
             smLaptop:absolute 
             smLaptop:w-full 
             smLaptop:justify-between
@@ -186,7 +186,7 @@ const Home = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <FaTwitterSquare className="w-[1.1rem] h-[1.1rem]" />
+            <FaTwitterSquare className="h-[1.1rem] w-[1.1rem]" />
           </a>
         </div>
       </footer>
--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,18 +1,18 @@
 declare module '*.jpg' {
-  export default '' as string;
+  export default '' as string
 }
 
 declare module '*.png' {
-  export default '' as string;
+  export default '' as string
 }
 
 declare module '*.svg' {
-  export default '' as any;
+  export default '' as unknown
 }
 
 declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
+  import React = require('react')
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
 }
